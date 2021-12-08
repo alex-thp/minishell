0db4ca9b3695121532daaee73544bcfe980f2e77
Alexandre DE TEMMERMAN <ade-temm@e3r4p14.42.fr>
2021-12-08 17:38:37 +0100

    arbre

--- main.c
+++ main.c
@@ -6,15 +6,191 @@
 /*   By: ade-temm <ade-temm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:26:48 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/12/03 18:04:53 by adylewsk         ###   ########.fr       */
+/*   Updated: 2021/12/08 17:31:21 by ade-temm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	interpret_command(char *command)
+int		check_pipe_error(char *command) //return 1 = error || return 0 = ok
 {
-	printf("la commande : %s\n", command);
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	while (command[i] == 32)
+		i++;
+	if (command[i] == '|')
+		return(1);
+	while (command[i])
+	{
+		j = 0;
+		k = 1;
+		while(command[i + k] && command[i + k] != '|')
+		{
+			if (command[i + k] != 32)
+				j++;
+			k++;
+		}
+		if (j == 0 && command[i + k])
+			return (1);
+		i += k;
+	}
+	return (0);
+}
+
+char	**lexer(char *command)
+{
+	int		i;
+	int		j;
+	char	**result;
+
+	i = 0;
+	j = 0;
+	if (check_pipe_error(command) == 1)
+	{
+		printf("error with pipe\n");
+		return (NULL);
+	}
+	result = ft_split(command, '|');
+	return (result);
+}
+int		is_special(char c, char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+char	*get_value2(char *str, int i)
+{
+	int	j;
+	char	*result;
+
+	str[i] = ' ';
+	while (str[i] == ' ')
+		i++;
+	j = 0;
+	while (str[i + j] && is_special(str[i + j], " <>") == 0)
+		j++;
+	result = malloc(sizeof(char) * (j + 1));
+	j = 0;
+	while (str[i + j] && is_special(str[i + j], " <>") == 0)
+	{
+		result[j] = str[i + j];
+		str[i + j] = ' ';
+		j++;
+	}
+	result[j] = 0;
+	return (result);
+}
+
+t_redirection	*create_redir(char *command)
+{
+	int				i;
+	int				j;
+	int				fd;
+	t_redirection	*redir;
+	char			*to_open;
+
+	redir = (t_redirection *)malloc(sizeof(t_redirection));
+	redir->stdin = -2;
+	redir->stdout = -2;
+	redir->stdin_file = NULL;
+	i = 0;
+	while (command[i] != 0)
+	{
+		if (command[i] == '<' || command[i] == '>' )
+		{
+			j = 0;
+			if (command[i] == '<')
+			{
+				if (redir->stdin_file)
+					free(redir->stdin_file);
+				redir->stdin_file = get_value2(command, i);
+			}
+			else if (command[i] == '>')
+			{
+				to_open = get_value2(command, i);
+				fd = open(to_open, O_CREAT | O_RDWR | O_TRUNC, 00664);
+				free(to_open);
+				redir->stdout = fd;
+			}
+		}
+		i++;
+	}
+	return (redir);
+}
+
+t_command	*create_cmd(char *command)
+{
+	t_command	*cmd;
+	char		**args;
+
+	cmd = (t_command *)malloc(sizeof(t_command));
+	args = ft_split(command, ' ');
+	if (args)
+	{
+		cmd->name = args[0];
+		cmd->args = args;
+	}
+	return (cmd);
+
+}
+
+t_node	*create_node(char *command)
+{
+	t_node	*node;
+
+	node = (t_node *)malloc(sizeof(t_node));
+	node->redir = create_redir(command);
+	node->cmd = create_cmd(command);
+	// printf("cmd->name %s\nstdin: %s\ncmd->args :\n", node->cmd->name, node->redir->stdin_file);
+	// ft_puttab(node->cmd->args);
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
+
+t_node	*create_tree(char **command)
+{
+	int		i;
+	t_node	*head;
+
+	i = 0;
+	while(command[i])
+		i++;
+	while (i > 0)
+	{
+		i--;
+		head = create_node(command[i]);
+	}
+	return (head);
+}
+
+void	interpret_command(char *command, t_datas *datas)
+{
+	char	**parsed_command;
+
+	parsed_command = lexer(command);
+	if (parsed_command == NULL)
+		return ;
+	datas->head = create_tree(parsed_command);
+	// while (*parsed_command)
+	// {
+	// 	printf("%s\n", *parsed_command);
+	// 	parsed_command++;
+	// }
+	//printf("la commande : %s\n", command);
 }
  //print l'env : printf("%s\n", *(env->g_env));
 
@@ -42,12 +218,15 @@ int		main(int ac, char **av, char **envp)
 	while (!quit)
 	{
 		command = readline("Minishell $> ");
-		add_history(command);
-		interpret_command(command);
 		quit = (command && command[0] == 'q' && !command[1]) || !command;
+		if (!quit)
+		{
+			add_history(command);
+			interpret_command(command, datas);
+		}
 		free(command);
 	}
-	rl_clear_history();
+	//rl_clear_history();
 	ft_freetab(datas->env);
 	free(datas);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ade-temm <ade-temm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 10:18:55 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/12/01 20:17:05 by adylewsk         ###   ########.fr       */
+/*   Updated: 2021/12/08 16:55:08 by ade-temm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,42 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "libft/libft.h"
-//typedef struct s_ptr_env
-//{
-//	struct s_env *first;
-//	struct s_env *last;
-//} t_ptr_env;
-//
-//typedef struct s_env
-//{
-//	char			*name;
-//	char			*value;
-//	struct s_env	*next;
-//    struct s_env	*previous;
-//}				t_env;
+
+/*
+* struct ENV
+*/
 
 typedef struct s_datas
 {
-	char	**env;
-	int		len_env;
+	char			**env;
+	int				len_env;
+	struct s_node	*head;
 }				t_datas;
+
+/*
+* struct ARBRE
+*/
+
+typedef struct	s_node
+{
+	struct s_command 		*cmd;
+	struct s_redirection 	*redir;
+	struct s_node 			*left;
+	struct s_node 			*right;
+}				t_node;
+
+typedef struct	s_command
+{
+	char	*name;
+	char	**args;
+}				t_command;
+
+typedef struct s_redirection
+{
+	char	*stdin_file;
+	int		stdin;
+	int		stdout;
+}				t_redirection;
 
 /*
 * manage_env.c
