d5f4479b393a623a5dd44e57528ef0d3d3974362
adylewsk <adylewsk@student.42.fr>
2021-12-17 17:53:05 +0100

    test

--- interpret.c
+++ interpret.c
@@ -6,33 +6,51 @@
 /*   By: adylewsk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 16:28:17 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/12/16 18:36:38 by adylewsk         ###   ########.fr       */
+/*   Updated: 2021/12/17 17:51:15 by adylewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	child(int *pip, t_node *head, t_datas *datas)
+void	parent(int *pip, t_node *head, t_datas *datas)
 {
-	dup2(pip[1], STDOUT_FILENO);
+	if (head->right->redir->fd_out >= 0)
+	{
+		dup2(head->right->redir->fd_out, STDOUT_FILENO);
+	}
+	else
+		dup2(pip[1], STDOUT_FILENO);
 	close(pip[0]);
 	if (head->left)
 		execute_tree(head->left, datas);
 }
 
-void	parent(int *pip, t_node *head, t_datas *datas)
+void	child(int *pip, t_node *head, t_datas *datas)
 {
-	int		status;
+//	int		status;
 	char	*cmd;
 
+	if (head->redir->fd_in >= 0)
+	{
+		dup2(head->redir->fd_in, STDIN_FILENO);
+	}
+	else
+		dup2(pip[0], STDIN_FILENO);
+	close(pip[1]);
+//	waitpid(-1, &status, 0);
+	cmd = check_exe(head->cmd->name, datas->env);
+	execve(cmd, head->cmd->args, datas->env);
+	free(cmd);
+	exit(0);
+}
+
+void	first_cmd(t_node *head, t_datas *datas)
+{
+	char	*cmd;
 	if (head->redir->fd_out >= 0)
 		dup2(head->redir->fd_out, STDOUT_FILENO);
 	if (head->redir->fd_in >= 0)
 		dup2(head->redir->fd_in, STDIN_FILENO);
-	else
-		dup2(pip[0], STDIN_FILENO);
-	close(pip[1]);
-	waitpid(-1, &status, 0);
 	cmd = check_exe(head->cmd->name, datas->env);
 	execve(cmd, head->cmd->args, datas->env);
 	free(cmd);
@@ -44,17 +62,22 @@ void	execute_tree(t_node *head, t_datas *datas)
 	int		pip[2];
 	int		pid;
 
-	pipe(pip);
 	if (!(head->cmd->name))
+	{
+		pipe(pip);
+
+		pid = fork();
+		if (pid == 0)
+			child(pip, head->right, datas);
+		else
+			parent(pip, head, datas);
+	}
+	else
 	{
 		pid = fork();
 		if (pid == 0)
-			child(pip, head, datas);
-		else
-			parent(pip, head->right, datas);
+			first_cmd(head, datas);
 	}
-	else
-		parent(pip, head, datas);
 	close(pip[0]);
 	close(pip[1]);
 }
@@ -62,7 +85,7 @@ void	execute_tree(t_node *head, t_datas *datas)
 void	interpret_command(char *command, t_datas *datas)
 {
 	char	**parsed_command;
-	int		pid;
+//	int		pid;
 
 	parsed_command = lexer(command);
 	if (parsed_command == NULL)
@@ -71,13 +94,13 @@ void	interpret_command(char *command, t_datas *datas)
 		datas->head = create_tree(parsed_command);
 	else
 		datas->head = create_node_command(&command[0]);
-	pid = fork();
-	if (pid == 0)
-	{
+//	pid = fork();
+//	if (pid == 0)
+//	{
 		execute_tree(datas->head, datas);
-		exit(0);
-	}
-	else
-		waitpid(0, &pid, 0);
+//		exit(0);
+//	}
+//	else
+		waitpid(-1, NULL, 0);
 	ft_freetab(parsed_command);
 }
