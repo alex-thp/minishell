a89dbae25d75544c82c791b18e23e0c2885a698b
adylewsk <adylewsk@student.42.fr>
2021-12-20 19:29:51 +0100

    cat | ls ok

--- interpret.c
+++ interpret.c
@@ -6,7 +6,7 @@
 /*   By: adylewsk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 16:28:17 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/12/20 18:31:29 by adylewsk         ###   ########.fr       */
+/*   Updated: 2021/12/20 19:28:59 by adylewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	child(int *pip, t_node *head, t_datas *datas)
 	dup2(pip[1], STDOUT_FILENO);
 	close(pip[0]);
 	execute_tree(head->left, datas);
+	exit(0);
 }
 
 void	parent(int *pip, t_node *head, t_datas *datas)
@@ -29,6 +30,7 @@ void	parent(int *pip, t_node *head, t_datas *datas)
 		dup2(head->right->redir->fd_in, STDIN_FILENO);
 	dup2(pip[0], STDIN_FILENO);
 	close(pip[1]);
+//	waitpid(-1, NULL, 0);
 	cmd = check_exe(head->right->cmd->name, datas->env);
 	execve(cmd, head->right->cmd->args, datas->env);
 	free(cmd);
@@ -39,6 +41,7 @@ void	first(t_node *head, t_datas *datas)
 {
 	char	*cmd;
 
+	waitpid(-1, NULL, 0);
 	if (head->redir->fd_out >= 0)
 		dup2(head->redir->fd_out, STDOUT_FILENO);
 	if (head->redir->fd_in >= 0)
@@ -54,26 +57,30 @@ void	execute_tree(t_node *head, t_datas *datas)
 	int		pip[2];
 	int		pid;
 
+	pipe(pip);
 	if (!head->cmd->name)
 	{
-		pipe(pip);
 		pid = fork();
 		if (pid == 0)
 		{
-			child(pip, head, datas);
+			parent(pip, head, datas);
 		}
 		else
-			parent(pip, head, datas);
+		{
+			child(pip, head, datas);
+//			waitpid(-1, NULL, 0);
+		}
 	}
 	else
 	{
 		pid = fork();
 		if (pid == 0)
 			first(head, datas);
+		waitpid(-1, NULL, 0);
 	}
 	close(pip[0]);
 	close(pip[1]);
-	exit(0);
+	waitpid(-1, NULL, 0);
 }
 
 int	interpret_command(char *command, t_datas *datas)
@@ -90,7 +97,11 @@ int	interpret_command(char *command, t_datas *datas)
 		datas->head = create_node_command(&command[0]);
 	pid = fork();
 	if (pid == 0)
+	{
 		execute_tree(datas->head, datas);
+//		waitpid(-1, NULL, 0);
+	}
+	waitpid(-1, NULL, 0);
 //	waitpid(pid, NULL, 0); // attendre la fin de tout les processus
 	ft_freetab(parsed_command);
 	return (1);
